0e5b01ea30f5c25785f4098618116234e6c222e5
ItamarBarHod <itamarbarhod@gmail.com>
2024-06-11 17:33:37 +0300

    rebase

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,17 +1,9 @@
 import { Link } from "react-router-dom";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import HomeIcon from "@mui/icons-material/Home";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import PersonIcon from "@mui/icons-material/Person";
-import AssignmentIcon from "@mui/icons-material/Assignment";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./Theme";
-import { AuthContext } from "../context/AuthProvider";
+import { AppBar, Toolbar, IconButton, Typography, ThemeProvider } from "@mui/material";
+import { Home as HomeIcon, AccountCircle as AccountCircleIcon, ExitToApp as ExitToAppIcon, Person as PersonIcon, Assignment as AssignmentIcon } from "@mui/icons-material";
 import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import theme from "./Theme";
 
 const Navbar = () => {
   const authContext = useContext(AuthContext);
--- Frontend/src/pages/EditGamingPreferencesPage.tsx
+++ Frontend/src/pages/EditGamingPreferencesPage.tsx
@@ -1,128 +1,127 @@
+import React, { useState } from "react";
 import {
-  Avatar,
   Box,
   Grid,
-  Typography,
-  Container,
   Button,
-  ThemeProvider,
-  Divider,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { User, UserPreferences } from "../api/types";
-import { getUserByToken } from "../api/api.endpoints";
-import theme from "../components/Theme";
+import { IoSearch } from "react-icons/io5";
 import GameSelector from "../components/GameSelector";
 import PlatformSelector from "../components/PlatformSelector";
-import VoiceSelector from "../components/VoiceSelector";
+import RegionSelector from "../components/RegionSelector";
 import SoloOrGroupSelector from "../components/SoloOrGroupSelector";
 import PreferedGenderSelector from "../components/PreferedGenderSelector";
-import RegionSelector from "../components/RegionSelector";
 import AgeRangeSelector from "../components/AgeRangeSelector";
+import VoiceSelector from "../components/VoiceSelector";
+import { AuthContext } from "../context/AuthProvider";
+import { useContext } from "react";
+import { UserPreferences, Gender, SoloOrGroup, Platform } from "../api/types";
 
-const EditGamingPreferencesPage = () => {
-  const navigate = useNavigate();
+const EditGamingPreferencesSection = () => {
+  const authContext = useContext(AuthContext);
+  const [userPref, setUserPref] = useState<UserPreferences | undefined>(
+    authContext?.user?.preferences
+  );
 
-  const handleSaveClick = () => {};
+  const handleClick = (fieldName: keyof UserPreferences, value: any) => {
+    setUserPref((prev) => ({
+      ...prev,
+      [fieldName]: value,
+    }));
+  };
 
-  const [user, setUser] = useState<User | undefined>(undefined);
-  const [userPref, setUserPref] = useState<UserPreferences>();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const userData = await getUserByToken();
-      setUser(userData);
-      setUserPref(userData?.preferences);
-    };
-
-    fetchUser();
-  }, []);
   return (
-    user && (
-      <Grid
-        // main Grid of all the section
-        sx={{
-          alignItems: "start",
-          p: 2,
-        }}
-      >
-        <Typography variant="h6" color="white" fontSize={40} marginRight={2}>
-          Gaming Preferences
-        </Typography>
-        <ThemeProvider theme={theme}>
-          <Grid sx={{ marginTop: 4, width: "50%" }}>
-            <Box
-              color={theme.palette.text.primary}
-              bgcolor="background.default"
-              p={2}
-              borderRadius={4}
-              display="flex"
-              flexDirection="column"
+    userPref && (
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12}>
+          <Box
+            bgcolor="background.default"
+            p={2}
+            borderRadius={4}
+            display="flex"
+            flexDirection="column"
+          >
+            {/* the Box that hold all fields */}
+            <GameSelector />
+            <PlatformSelector
+              label="Select Platform"
+              selectedPlatforms={userPref.platform ? userPref.platform : []}
+              onChange={(event, newPlatform: Platform) =>
+                handleClick("platform", newPlatform)
+              }
+            />
+            <RegionSelector
+              region={userPref.region ? userPref.region : ""}
+              onChange={(event, newRegion: string) =>
+                handleClick("region", newRegion)
+              }
+              label="Select Region"
+            />
+            <SoloOrGroupSelector
+              label="Are you searching alone or with other group?"
+              soloOrGroup={
+                userPref.soloOrGroup ? userPref.soloOrGroup : SoloOrGroup.None
+              }
+              onChange={(event, newSoloOrGroup: SoloOrGroup) =>
+                handleClick("soloOrGroup", newSoloOrGroup)
+              }
+            />
+            <PreferedGenderSelector
+              label="Which gender do you prefer to play with?"
+              selectedGender={
+                userPref.prefGender ? userPref.prefGender : Gender.None
+              }
+              onChange={(event, newGender: Gender) =>
+                handleClick("prefGender", newGender)
+              }
+            />
+            <PlatformSelector
+              label="Select teammate Platform"
+              selectedPlatforms={
+                userPref.teammatePlatform ? userPref.teammatePlatform : []
+              }
+              onChange={(event, newTeammatePlatform: Platform[]) =>
+                handleClick("teammatePlatform", newTeammatePlatform)
+              }
+            />
+            <AgeRangeSelector
+              label="Between what ages are your ideal teammates?"
+              minAge={userPref.ageRange ? userPref.ageRange[0] : 18}
+              maxAge={userPref.ageRange ? userPref.ageRange[1] : 90}
+              onChange={(min: number, max: number) =>
+                handleClick("ageRange", [min, max])
+              }
+            />
+            <VoiceSelector
+              isVoice={userPref.voice ? userPref.voice : undefined}
+              onChange={(event, isVoice: boolean) =>
+                handleClick("voice", isVoice)
+              }
+            />
+            <Button
+              startIcon={<IoSearch />}
+              variant="contained"
+              size="medium"
+              sx={{
+                width: "30%",
+                fontWeight: "bold",
+                fontSize: "20px",
+                backgroundColor: "#555555",
+                color: "#BBBBBB",
+                border: "1px solid transparent",
+                "&:hover": {
+                  color: "white",
+                  backgroundColor: "#222222",
+                  border: "1px solid white",
+                },
+              }}
             >
-              {/* the Box that hold all fields */}
-              <GameSelector />
-              <PlatformSelector
-                label="Select Platform"
-                selectedPlatforms={user.preferences?.platform}
-                onChange={handlePlatformChange}
-              />
-              <RegionSelector
-                region={searchProps.region}
-                onChange={handleRegionChange}
-                label="Select Region"
-              />
-              <SoloOrGroupSelector
-                label="Are you searching alone or with other group?"
-                soloOrGroup={searchProps.soloOrGroup}
-                onChange={handleSoloGroupChange}
-              />
-              <PreferedGenderSelector
-                label="Which gender do you prefer to play with?"
-                selectedGender={searchProps.prefGender}
-                onChange={handleGenderChange}
-              />
-              <PlatformSelector
-                label="Select teammate Platform"
-                selectedPlatforms={searchProps.teammatePlatform}
-                onChange={handleTeammatePlatformChange}
-              />
-              <AgeRangeSelector
-                label="Between what ages are your ideal teammates?"
-                minAge={searchProps.ageRange.minAge}
-                maxAge={searchProps.ageRange.maxAge}
-                onChange={handleAgeRangeChange}
-              />
-              <VoiceSelector
-                isVoice={searchProps.isVoice}
-                onChange={handleVoiceClick}
-              />
-              <Button
-                startIcon={<IoSearch />}
-                variant="contained"
-                size="medium"
-                sx={{
-                  width: "30%",
-                  fontWeight: "bold",
-                  fontSize: "20px",
-                  backgroundColor: "#555555",
-                  color: "#BBBBBB",
-                  border: "1px solid transparent",
-                  "&:hover": {
-                    color: "white",
-                    backgroundColor: "#222222",
-                    border: "1px solid white",
-                  },
-                }}
-              >
-                Search
-              </Button>
-            </Box>
-          </Grid>
-        </ThemeProvider>
+              Search
+            </Button>
+          </Box>
+        </Grid>
       </Grid>
     )
   );
 };
 
-export default EditGamingPreferencesPage;
+export default EditGamingPreferencesSection;
